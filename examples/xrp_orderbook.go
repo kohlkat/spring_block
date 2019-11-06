84bda6bda4281005f3ced632aee491b1adcb5591
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2019-11-06 23:26:00 +0100

    Add example websocket request for order book

---
+++ examples/xrp_orderbook.go
@@ -0,0 +1,109 @@
+package main
+
+// Basic exemple usage of https://xrpl.org/websocket-api-tool.html#book_offers
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+// websocket address
+var addr = flag.String("addr", "s1.ripple.com:51233", "http service address")
+
+// Payload object
+type message struct {
+	Command string `json:"command"`
+	//Taker     string    `json:"taker"`
+	TakerGets takerGets `json:"taker_gets"`
+	TakerPays takerPays `json:"taker_pays"`
+	Limit     uint      `json:"limit"`
+}
+
+type takerGets struct {
+	Currency string `json:"currency"`
+}
+
+type takerPays struct {
+	Currency string `json:"currency"`
+	Issuer   string `json:"issuer"`
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	var m message
+
+	// check for interrupts and cleanly close the connection
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/"}
+	log.Printf("connecting to %s", u.String())
+
+	// make the connection
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	// on exit close
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	// Exemple values
+	m.Command = "book_offers"
+	//m.Taker = "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59"
+	m.TakerGets.Currency = "XRP"
+	m.TakerPays.Currency = "USD"
+	m.TakerPays.Issuer = "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"
+	m.Limit = 10
+
+	// struct to JSON marshalling
+	msg, _ := json.Marshal(m)
+	// write to the websocket
+	err = c.WriteMessage(websocket.TextMessage, []byte(string(msg)))
+	if err != nil {
+		log.Println("write:", err)
+		return
+	}
+
+	// read from the websocket
+	_, message, err := c.ReadMessage()
+	if err != nil {
+		log.Println("read:", err)
+		return
+	}
+	// print the response from the XRP Ledger
+	log.Printf("recv: %s", message)
+
+	// handle interrupt
+	for {
+		select {
+		case <-done:
+			return
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+}
