82812b532b99235e7f0e617d02a0f439eb5be3d7
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2019-11-13 13:35:31 +0100

    Fix ASCII art

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	display()
 
-	s.LaunchServer()
+	go s.LaunchServer()
 
 	var addr = flag.String("addr", "s1.ripple.com:51233", "http service address")
 	xrpapi.XrpGetLedgerSeq(addr)
@@ -19,7 +19,7 @@ func main() {
 
 func display() {
 	fmt.Println(`
-____  ____  ____  __  __ _   ___    ____  __     __    ___  __ _    __     __   ____  ____ 
+ ____  ____  ____  __  __ _   ___    ____  __     __    ___  __ _    __     __   ____  ____ 
 / ___)(  _ \(  _ \(  )(  ( \ / __)  (  _ \(  )   /  \  / __)(  / )  (  )   / _\ (  _ \/ ___)
 \___ \ ) __/ )   / )( /    /( (_ \   ) _ (/ (_/\(  O )( (__  )  (   / (_/\/    \ ) _ (\___ \
 (____/(__)  (__\_)(__)\_)__) \___/  (____/\____/ \__/  \___)(__\_)  \____/\_/\_/(____/(____/
