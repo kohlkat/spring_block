137d1b226352b2a41cd32936e0a694141a6a48b0
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2019-11-13 13:29:05 +0100

    Add main and program start

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"flag"
+
+	xrpapi "github.com/gaspardpeduzzi/spring_block/data"
+)
+
+func main() {
+	var addr = flag.String("addr", "s1.ripple.com:51233", "http service address")
+	xrpapi.XrpGetLedgerSeq(addr)
+
+}
