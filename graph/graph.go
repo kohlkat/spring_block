bf46163cb8b97bdbcce2ab02f74b04801ed8cb89
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2019-11-15 15:34:02 +0100

    Add debug tag for more invastigation

--- graph/graph.go
+++ graph/graph.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"log"
 	"math"
 	"sort"
 	"sync"
@@ -77,9 +78,9 @@ func (graph *Graph) CreateSimpleGraph() SimplerGraph {
 
 	for k1, v1 := range graph.Graph {
 		for k2, v2 := range v1 {
-			// if len(v2.List) > 2 {
-			// 	log.Println("check", v2.List[0].Rate >= v2.List[1].Rate)
-			// }
+			if len(v2.List) > 2 {
+				log.Println("check", v2.List[0].Rate >= v2.List[1].Rate)
+			}
 			simpleGraph[k1][k2] = -math.Log(v2.List[0].Rate)
 		}
 	}
